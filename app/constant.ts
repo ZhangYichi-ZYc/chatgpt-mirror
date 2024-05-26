a47f32149ff92bca15710047b11dfbc487ef52bd
ZhangYichi <admin@zhangyichi.cn>
2024-05-26 17:00:06 +0800

    调整KnowledgeCutOffDate

--- app/constant.ts
+++ app/constant.ts
@@ -123,29 +123,19 @@ export const SUMMARIZE_MODEL = "gpt-3.5-turbo";
 export const GEMINI_SUMMARIZE_MODEL = "gemini-pro";
 
 export const KnowledgeCutOffDate: Record<string, string> = {
-  default: "2021-09",
-  "gpt-4-turbo": "2023-12",
-  "gpt-4-turbo-2024-04-09": "2023-12",
-  "gpt-4-turbo-preview": "2023-12",
-  "gpt-4o": "2023-10",
-  "gpt-4o-2024-05-13": "2023-10",
-  "gpt-4-vision-preview": "2023-04",
-  // After improvements,
-  // it's now easier to add "KnowledgeCutOffDate" instead of stupid hardcoding it, as was done previously.
-  "gemini-pro": "2023-12",
-  "gemini-pro-vision": "2023-12",
+  default: "2024-05",
 };
 
 const openaiModels = [
-  "gpt-3.5-turbo",
-  "gpt-4",
-  "gpt-4-turbo",
-  "gpt-4o",
-  "claude-3-sonnet",
-  "claude-3-opus",
-  "claude-3-haiku",
-  "gemini-1.5-pro",
-  "gemini-1.5-flash"
+  "GPT-3.5 Turbo",
+  "GPT-4",
+  "GPT-4 Turbo",
+  "GPT-4o",
+  "Claude 3 Sonnet",
+  "Claude 3 Opus",
+  "Claude 3 Haiku",
+  "Gemini 1.5 Pro",
+  "Gemini 1.5 Flash"
 ];
 
 const googleModels = [
