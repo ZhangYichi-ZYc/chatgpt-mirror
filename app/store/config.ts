d85d7f94c65403c7cde313189af14a0cf92d3c6c
ZhangYichi <admin@zhangyichi.cn>
2024-05-26 17:06:12 +0800

    调整默认参数

--- app/store/config.ts
+++ app/store/config.ts
@@ -47,10 +47,10 @@ export const DEFAULT_CONFIG = {
   models: DEFAULT_MODELS as any as LLMModel[],
 
   modelConfig: {
-    model: "gpt-4o" as ModelType,
+    model: "GPT-4o" as ModelType,
     temperature: 0.5,
     top_p: 1,
-    max_tokens: 4000,
+    max_tokens: 510000,
     presence_penalty: 0,
     frequency_penalty: 0,
     sendMemory: true,
