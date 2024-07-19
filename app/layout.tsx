f3d482d50e121dbb5ae7a5b6a31632ca3b023e02
ZhangYichi <admin@zhangyichi.cn>
2024-07-19 17:34:19 +0800

    网页标题、SEO、侧边栏

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,10 +10,10 @@ import { GoogleTagManager } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "ChatGPT",
+  description: "Shaping the future of technology.",
   appleWebApp: {
-    title: "NextChat",
+    title: "ChatGPT",
     statusBarStyle: "default",
   },
 };
