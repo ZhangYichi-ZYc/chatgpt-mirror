2ededa6456d77254865c6d29922794bcaf9c506a
ZhangYichi <admin@zhangyichi.cn>
2024-09-14 15:57:48 +0800

    适配o1判定

--- app/client/platforms/openai.ts
+++ app/client/platforms/openai.ts
@@ -160,7 +160,7 @@ export class ChatGPTApi implements LLMApi {
     let requestPayload: RequestPayload | DalleRequestPayload;
 
     const isDalle3 = _isDalle3(options.config.model);
-    const isO1 = options.config.model.startsWith("o1");
+    const isO1 = options.config.model.startsWith("OpenAI");
     if (isDalle3) {
       const prompt = getMessageTextContent(
         options.messages.slice(-1)?.pop() as any,
